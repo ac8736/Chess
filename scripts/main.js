c4cb7a2bf84c0d6dab9d6eb776fc9832115cd29f
chena <chena@DESKTOP-6IV548E>
2021-12-31 21:28:41 -0500

    ref

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
-oops = 5;
-console.log(oops);
+// querySelectorAll("div[has-piece]");
+
+// const place = document.getElementById("a2");
+// place.addEventListener("click", function () {
+//   document.getElementById("a3").classList.add("moveable");
+// });
+
+// function move() {
+//   if (document.getElementById("a3").classList.contains("moveable")) {
+//     place.removeChild(place.children[0]);
+//     let img = document.createElement("img");
+//     img.src = "../sprites/whitePawn.png";
+//     img.classList.add("resize");
+//     document.getElementById("a3").appendChild(img);
+//   }
+// }
+
+// function move() {
+//   const pieces = document.querySelectorAll("div[has-piece]"); //returns a list of all the divs with a attribute has-piece
+//   pieces[0].removeChild(pieces[0].children[0]);  // .children returns a list of all the children of a node
+// }
+
+// appendChild() to add a child element, first do createElement('element name')
+// .classList.add() to add a class
